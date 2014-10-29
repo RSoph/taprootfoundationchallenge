5de654f7cda4ebe1b8aecd55810500d7b85cbf8b
Kate Brender <kate.brender@gmail.com>
2014-10-29 08:21:52 -0400

    map pins working

--- app/app/controllers/maps_controller.rb
+++ app/app/controllers/maps_controller.rb
@@ -9,6 +9,11 @@ class MapsController < ApplicationController
 		if Map.findings(@location)["events"]
 			@nearby_events = Map.findings(@location)["events"]["event"]
 		end
+		@locations_hash = Gmaps4rails.build_markers(@nearby_events) do |event, marker|
+  		marker.lat event["venue"]["location"]["geo:point"]["geo:lat"]
+  		marker.lng event["venue"]["location"]["geo:point"]["geo:long"]
+			marker.infowindow event["title"] + "<br />" + event["venue"]["name"] + "<br />" + event["venue"]["location"]["street"]
+		end
 	end
 
 	private
