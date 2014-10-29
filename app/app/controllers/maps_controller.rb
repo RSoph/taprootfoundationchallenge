2ba4ac21ccad16d0e6eb9c35dfd5a3a7dfae367d
Kate Brender <kate.brender@gmail.com>
2014-10-29 16:19:55 -0400

    twitter button working

--- app/app/controllers/maps_controller.rb
+++ app/app/controllers/maps_controller.rb
@@ -8,6 +8,10 @@ class MapsController < ApplicationController
 		if Map.findings(@location)["events"]
 			@nearby_events = Map.findings(@location)["events"]["event"]
 		end
+		# The locations_hash below takes in certain data from each event,
+		# in this case, lat and long corrdinates, the event title (usually the artist's name)
+		# the venue name, and address. This will be given to the google map
+		# in the view so the pins can be plotted
 		@locations_hash = Gmaps4rails.build_markers(@nearby_events) do |event, marker|
   		marker.lat event["venue"]["location"]["geo:point"]["geo:lat"]
   		marker.lng event["venue"]["location"]["geo:point"]["geo:long"]
